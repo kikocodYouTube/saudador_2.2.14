5abcb32ebd64cb7b87a6da8a96dc743f9a2dc916
Francisco Tejero <55576376+kikocodYouTube@users.noreply.github.com>
2019-09-26 09:40:24 +0200

    Add files via upload

---
+++ saludador_2.2.14.py
@@ -0,0 +1,6 @@
+def saludador_2 () :
+	print "Hola amigo" #Instruccióon de salida
+	nombre= raw_input("Como te llamas?: ")  #Instrucción de salida
+	print "Bienvenido a mi casa" , nombre
+
+saludador_2()
